572074cdb751b8e0d827c300acca4a25d8b48739
Yadav Rohit <156703479+Rohit-Yadav-Officaial@users.noreply.github.com>
2024-01-16 01:46:08 +0530

    Update todo.jsx

--- todo-app/src/todo.jsx
+++ todo-app/src/todo.jsx
@@ -1,42 +1,60 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { v4 as uuidv4 } from 'uuid';
-export default function ToDo(){
- let [todo,setTodo]=useState([{task: "sample task" ,id:uuidv4()}]);
- let [newtodo,setnewTodo]=useState("");
-  
-  let updatetodo=()=>{
-    setTodo([...todo,{ task: newtodo, id: uuidv4() }]);
+export default function ToDo() {
+  let [todo, setTodo] = useState([{ task: "sample task", id: uuidv4(), complete: false }]);
+  let [newtodo, setnewTodo] = useState("");
+
+  let updatetodo = () => {
+    setTodo([...todo, { task: newtodo, id: uuidv4(), complete: false }]);
     setnewTodo("");
   };
 
- let updatetodovalue=(event)=>{
+  let updatetodovalue = (event) => {
     setnewTodo(event.target.value);
- };
- let deleteTodo=(id)=>{
-   let copy=setTodo((prevTodo) => prevTodo.filter((t) => t.id !== id));
-   console.log(copy);
- };
-return(<div>
-    <h2>to-do</h2>
-     <input type="text" placeholder="enter task" value={newtodo} onChange={updatetodovalue}/>
-    
-     
-     <button type ="submit" onClick={updatetodo}> add+</button>
-     
-    <ul>
-        {todo.map((todo)=>( 
-            <li key={todo.id}>
-                <span>{todo.task}</span>
-                &nbsp; &nbsp; &nbsp;
-                <button onClick={() => deleteTodo(todo.id)} >delete</button>
-            
-            
-            </li>
-            
-            ))
-        }
-    </ul>
+  };
 
- </div>);
+  let deleteTodo = (id) => {
+    setTodo((prevTodo) => prevTodo.filter((t) => t.id !== id));
+  };
 
+  let completetask = (id) => {
+    setTodo((prevTodo) =>
+      prevTodo.map((t) =>
+        t.id === id ? { ...t, complete: !t.complete } : t
+      )
+    );
+  };
+
+  return (
+    <div>
+      <h2>to-do</h2>
+      <input
+        type="text"
+        placeholder="enter task"
+        value={newtodo}
+        onChange={updatetodovalue}
+      />
+      &nbsp; &nbsp; &nbsp;
+      <button type="submit" onClick={updatetodo}>
+        add+
+      </button>
+      <ul>
+        {todo.map((todo) => (
+          <li key={todo.id}>
+            <input
+              type="checkbox"
+              checked={todo.complete}
+              onChange={() => completetask(todo.id)}
+            />
+            &nbsp; &nbsp; &nbsp;
+            <span style={{ color: todo.complete ? 'green' : 'black' }}>
+              {todo.task}
+            </span>
+            &nbsp; &nbsp; &nbsp;
+            <button onClick={() => deleteTodo(todo.id)} style={{color:'red'}}>delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
