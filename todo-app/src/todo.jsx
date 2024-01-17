bf0e92814acc144614f8b05fb87873282f092b4a
Yadav Rohit <156703479+Rohit-Yadav-Official@users.noreply.github.com>
2024-01-18 04:37:30 +0530

    Update todo.jsx

--- todo-app/src/todo.jsx
+++ todo-app/src/todo.jsx
@@ -5,6 +5,8 @@ export default function ToDo() {
   let [todo, setTodo] = useState([{ task: "sample task", id: uuidv4(), complete: false, priority: 0 }]);
   let [newtodo, setnewTodo] = useState("");
   let [priority, setpriority] = useState(0);
+  let [edittask, setedittask] = useState(null);
+  let [writingEditTask, setWritingEditTask] = useState("");
 
   let updatetodo = () => {
     setTodo([...todo, { task: newtodo, id: uuidv4(), complete: false, priority }]);
@@ -42,6 +44,24 @@ export default function ToDo() {
     }
   };
 
+  function startedit(id, task) {
+    setedittask(id);
+    setWritingEditTask(task);
+  }
+
+  function saveEditing(id) {
+    setTodo((prevTodo) =>
+      prevTodo.map((t) =>
+        t.id === id ? { ...t, task: writingEditTask } : t
+      )
+    );
+    setedittask(null);
+  }
+
+  function stopediting() {
+    setedittask(null);
+  }
+
   return (
     <div>
       <h2>to-do</h2>
@@ -56,21 +76,38 @@ export default function ToDo() {
         add+
       </button>
       <ul>
-        {todo.map((todo, index) => (
-          <li key={todo.id}>
+        {todo.map((t, index) => (
+          <li key={t.id}>
             <button onClick={() => changepriority(index)}>&#8593;</button>
             &nbsp; &nbsp; &nbsp;
             <input
               type="checkbox"
-              checked={todo.complete}
-              onChange={() => completetask(todo.id)}
+              checked={t.complete}
+              onChange={() => completetask(t.id)}
             />
             &nbsp; &nbsp; &nbsp;
-            <span style={{ color: todo.complete ? 'green' : 'black' }}>
-              {todo.task}
-            </span>
+
+            {edittask === t.id ? (
+              <>
+                <input
+                  type="text"
+                  value={writingEditTask}
+                  onChange={(e) => setWritingEditTask(e.target.value)}
+                />
+                <button onClick={() => saveEditing(t.id)}>save</button>-
+                <button onClick={stopediting}>cancel</button>
+              </>
+            ) : (
+              <>
+                <span style={{ color: t.complete ? 'green' : 'black' }}>
+                  {t.task}
+                </span>
+                <button onClick={() => startedit(t.id, t.task)}>edit</button>
+              </>
+            )}
             &nbsp; &nbsp; &nbsp;
-            <button onClick={() => deleteTodo(todo.id)} style={{ color: 'red' }}>delete</button>
+            <button onClick={() => deleteTodo(t.id)} style={{ color: 'red' }}>delete</button>
+            <br /><br />
           </li>
         ))}
       </ul>
